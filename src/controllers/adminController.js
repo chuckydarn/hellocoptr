65e7c237814267af121b8aca9d1c50a97c6f6d46
Charlie Haag <charlottehaag22@gmail.com>
2019-03-17 14:37:34 -0700

    completed dashboard styling and rerouting

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,6 @@
 module.exports = {
   index(req, res, next) {
-    if(!res.user) {
+    if(!req.user) {
       res.redirect("/admin/users/sign_in");
     } else {
       res.render("admin/dashboard");
